e120f9e70803f09bbffb8bd20a3943cfe312819a
Marc-Oliver Miekautsch <marc-oliver.miekautsch@iu-study.org>
2026-08-12 21:33:37 +0200

    Projektdateien hinzufügen.

---
+++ Kontaktverwalter/Utils/AsyncRelayCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Kontaktverwalter.Utils
+{
+    /// <summary>
+    /// Implementation of ICommand for async button commands and checks
+    /// </summary>
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<Task> _execute;
+        private readonly Func<bool> _canExecute;
+        private bool _isExecuting;
+
+        public AsyncRelayCommand(Func<Task> execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            // disable button while executing to prevent double clicking
+            if (_isExecuting) return false;
+
+            return _canExecute == null || _canExecute();
+        }
+
+        public async void Execute(object? parameter)
+        {
+            _isExecuting = true;
+            CommandManager.InvalidateRequerySuggested(); // Refresh UI
+
+            try
+            {
+                await _execute();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error occured:\n{ex}");
+            }
+            finally
+            {
+                _isExecuting = false;
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+    }
+}
---
+++ Kontaktverwalter/Utils/ObservableObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kontaktverwalter.Utils
+{
+    /// <summary>
+    /// Abstract base class for all objects that need to implement INotifyPropertyChanged, to avoid code duplication.
+    /// </summary>
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public virtual void RaisePropertyChangedEvent([CallerMemberName] string? propertyName = null)
+        {
+            if (propertyName != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(propertyName), "Property name cannot be null.");
+            }
+        }
+    }
+}
